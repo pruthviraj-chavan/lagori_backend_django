97e12bc8245371a7c46f31b372572bb3fa8431c4
Pruthviraj Chavan <91907487+pruthviraj-chavan@users.noreply.github.com>
2025-03-26 14:53:52 +0530

    Update asgi.py

--- lagori_backend/lagori_backend/asgi.py
+++ lagori_backend/lagori_backend/asgi.py
@@ -3,7 +3,7 @@ ASGI config for lagori_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see j
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
